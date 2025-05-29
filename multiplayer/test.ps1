bfe8a80928d1e4681260d00270f5b1b3bd697d04
lovebird <lovebirdsx@gmail.com>
2025-05-29 11:32:40 +0800

    升级到4.4.1

---
+++ multiplayer/test.ps1
@@ -0,0 +1,2 @@
+Start-Process -FilePath "Godot_v4.4.1-stable_win64.exe" -ArgumentList "--server", "--position", "0,0" -NoNewWindow
+Start-Process -FilePath "Godot_v4.4.1-stable_win64.exe" -ArgumentList "--position", "800,0" -NoNewWindow
